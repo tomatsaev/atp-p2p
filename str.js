00c5f21a701303e76d2ca8d85dd8b830cee384a4
tomatsaev <tom.matsaev@icloud.com>
2022-02-03 13:06:47 +0200

    Changes by orsaa

--- str.js
+++ str.js
@@ -1,13 +1,20 @@
 function handleDelete(string, index) {
-    return string.slice(0, index) + string.slice(index + 1)
+    let local_index = index;
+    const org_string = (' ' + string).slice(1);
+    let new_string = org_string.substr(0, local_index);
+    local_index++;
+    new_string = new_string + org_string.substr(local_index);
+    return new_string;
 }
 
 const handleInsert = (string, elements) => {
+    let new_string;
     if (elements.length > 1) {
-        return string.slice(0, elements[1]) + elements[0] + string.slice(elements[1])
+        new_string =  string.slice(0, elements[1]) + elements[0] + string.slice(elements[1])
     } else {
-        return elements[0] + string.slice(0)
+        new_string =  elements[0] + string.slice(0)
     }
+    return new_string;
 }
 
 module.exports = {handleDelete, handleInsert}
